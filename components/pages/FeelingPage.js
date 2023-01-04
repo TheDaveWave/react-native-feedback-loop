f5093e2f58db1217c72ba96829c17d0e780c3204
David Lindberg <david.wesley.lindberg@gmail.com>
2023-01-04 14:50:30 -0600

    started updating the style of the feeling page

--- components/pages/FeelingPage.js
+++ components/pages/FeelingPage.js
@@ -8,32 +8,41 @@ export default function FeelingPage({ navigation }) {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>How are you feeling?</Text>
-      <Picker
-        selectedValue={selectedValue}
-        onValueChange={function changeValue(itemValue, itemIndex) {
-          setSelectedValue(itemValue);
-        }}
-      >
-        <Picker.Item label="1" value={1} />
-        <Picker.Item label="2" value={2} />
-        <Picker.Item label="3" value={3} />
-        <Picker.Item label="4" value={4} />
-        <Picker.Item label="5" value={5} />
-      </Picker>
-      <Button title="Next" onPress={() => navigation.navigate("Support")} />
+      <View style={styles.body}>
+        <Text style={styles.header}>How are you feeling?</Text>
+        <Picker
+          selectedValue={selectedValue}
+          onValueChange={function changeValue(itemValue, itemIndex) {
+            setSelectedValue(itemValue);
+          }}
+        >
+          <Picker.Item label="1" value={1} />
+          <Picker.Item label="2" value={2} />
+          <Picker.Item label="3" value={3} />
+          <Picker.Item label="4" value={4} />
+          <Picker.Item label="5" value={5} />
+        </Picker>
+        <View style={styles.bottom}>
+          <Text>{selectedValue}</Text>
+          <Button title="Next" onPress={() => navigation.navigate("Support")} />
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      // backgroundColor: "#fff",
-      // alignItems: "center",
-      justifyContent: "center",
-    },
-    header: {
-      alignSelf: "center",
-    },
-  });
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  body: {
+    backgroundColor: "#EAF1E3",
+  },
+  header: {
+    alignSelf: "center",
+  },
+  bottom: {
+    alignItems: "center",
+  },
+});
