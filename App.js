3ed0c3fc77e530c0d0d74e9561a2e6e7b991a668
David Lindberg <david.wesley.lindberg@gmail.com>
2023-01-11 10:44:54 -0600

    added a provider to the app.js component

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
+import { Provider } from "react-redux";
 
 // component imports:
 import StackNavigator from "./components/navigators/StackNavigator";
+import store from "./redux/store";
 
 export default function App() {
   return (
-    <NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
         <StackNavigator />
         <StatusBar style="auto" />
-    </NavigationContainer>
+      </NavigationContainer>
+    </Provider>
   );
 }
