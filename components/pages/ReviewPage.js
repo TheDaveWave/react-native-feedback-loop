433f36d9e03eb121acf9d4a0f3e25a6d9309af97
David Lindberg <david.wesley.lindberg@gmail.com>
2023-01-11 09:09:11 -0600

    styled the review and success page to fit the style of the other screens

--- components/pages/ReviewPage.js
+++ components/pages/ReviewPage.js
@@ -1,17 +1,47 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ImageBackground } from "react-native";
 import CustomButton from "../buttons/CustomButton";
 
 const image = require("../../assets/dripglobe.jpeg");
 
 export default function ReviewPage({ navigation }) {
   return (
-    <View>
-      <Text>Review Feedback.</Text>
-      <Text>This will display the values chosen on the previous pages.</Text>
-      <CustomButton
-        title="Submit Feedback"
-        onPress={() => navigation.navigate("Success")}
-      />
+    <View style={styles.container}>
+      <ImageBackground source={image} style={styles.image} resizeMode="cover">
+        <View style={styles.body}>
+          <Text style={styles.bodyHeader}>Feedback Review</Text>
+          <Text style={styles.bodyText}>
+            This will display the values chosen on the previous pages.
+          </Text>
+          <CustomButton
+            title="Submit Feedback"
+            onPress={() => navigation.navigate("Success")}
+          />
+        </View>
+      </ImageBackground>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  body: {
+    width: "80%",
+    backgroundColor: "#F2F3D9",
+    borderRadius: 10,
+    padding: 30,
+  },
+  bodyHeader: {
+    alignSelf: "center",
+  },
+  bodyText: {
+    marginBottom: 15,
+    marginTop: 15,
+  },
+});
--- components/pages/SuccessPage.js
+++ components/pages/SuccessPage.js
@@ -1,20 +1,45 @@
-import { Button, View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ImageBackground } from "react-native";
 import CustomButton from "../buttons/CustomButton";
 
 const image = require("../../assets/dripglobe.jpeg");
 
-export default function SuccessPage({navigation}) {
+export default function SuccessPage({ navigation }) {
   return (
-    <View>
-      <Text>Feedback Submitted!</Text>
-      <CustomButton 
-        title="Submit New Feedback"
-        onPress={() => navigation.navigate("Feeling")}
-      />
-      <CustomButton
-        title="Home"
-        onPress={() => navigation.navigate("Home")}
-      />
+    <View style={styles.container}>
+      <ImageBackground source={image} style={styles.image} resizeMode="cover">
+        <View style={styles.body}>
+          <Text style={styles.bodyHeader}>Feedback Submitted!</Text>
+          <CustomButton
+            title="Submit New Feedback"
+            onPress={() => navigation.navigate("Feeling")}
+          />
+          <CustomButton
+            title="Home"
+            onPress={() => navigation.navigate("Home")}
+          />
+        </View>
+      </ImageBackground>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  body: {
+    // width: "80%",
+    backgroundColor: "#F2F3D9",
+    borderRadius: 10,
+    padding: 30,
+  },
+  bodyHeader: {
+    alignSelf: "center",
+    marginBottom: 10,
+  },
+});
