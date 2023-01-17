e3465ca847aec41190050f6e175c4e466db28cf9
David Lindberg <david.wesley.lindberg@gmail.com>
2023-01-17 16:56:55 -0600

    added login and logout routes to the user router

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const pool = require("../modules/pool.js");
 const router = express.Router();
+const {
+  rejectUnauthenticated,
+} = require("../modules/authentication-middleware.js");
+const userStrategy = require("../strategies/user.strategy.js");
+const encryptLib = require("../modules/encryption.js");
+
+// add authentication here
+router.get("/", (req, res) => {
+  // may not work - look into sessions
+  res.send(req.user);
+});
 
 // create a user account and store it onto the database.
-router.post("/register", (req, res) => {
+router.post("/register", (req, res, next) => {
   const { username } = req.body;
-  const password = req.body.password;
+  const password = encryptLib.encryptPassword(req.body.password);
   const queryText = `INSERT INTO "users" ("username", "password")
         VALUES ($1, $2);`;
   pool
@@ -17,6 +28,15 @@ router.post("/register", (req, res) => {
       console.log("Error registering user", err);
       res.send(500);
     });
+});
+
+router.post("/login", userStrategy.authenticate('local'), (req, res) => {
+    res.sendStatus(200);
+});
+
+router.post("/logout", (req, res) => {
+    req.logOut();
+    res.sendStatus(200);
 });
 
 module.exports = router;
