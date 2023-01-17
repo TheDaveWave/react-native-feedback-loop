cdaf6105d529f092b17ebf7a1397c04f849dd000
David Lindberg <david.wesley.lindberg@gmail.com>
2023-01-17 16:35:36 -0600

    added a user strategy file to the server folder

---
+++ server/strategies/user.strategy.js
@@ -0,0 +1,49 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const encryptLib = require("../modules/encryption.js");
+const pool = require("../modules/pool.js");
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  pool
+    .query(`SELECT * FROM "users" WHERE "id"=$1;`, [id])
+    .then((result) => {
+      const user = result && result.rows && result.rows[0];
+
+      if (user) {
+        delete user.password;
+        done(null, user);
+      } else {
+        done(null, null);
+      }
+    })
+    .catch((err) => {
+      console.log("Error with query during deserializing user ", err);
+      done(err, null);
+    });
+});
+
+passport.use(
+  "local",
+  new LocalStrategy((username, password, done) => {
+    pool
+      .query(`SELECT * FROM "users" WHERE "id"=$1;`, [username])
+      .then((result) => {
+        const user = result && result.rows && result.rows[0];
+        if (user && encryptLib.comparePassword(password, user.password)) {
+          done(null, user);
+        } else {
+          done(null, null);
+        }
+      })
+      .catch((err) => {
+        console.log("Error with query for user ", err);
+        done(err, null);
+      });
+  })
+);
+
+module.exports = passport;
