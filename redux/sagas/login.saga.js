7c8ceeeb32d843a2975b172477c83b573a4038ff
David Lindberg <david.wesley.lindberg@gmail.com>
2023-01-17 16:46:40 -0600

    added authentication middelware logic

---
+++ redux/sagas/login.saga.js
@@ -0,0 +1,19 @@
+import { put, takeLatest } from "redux-saga/effects";
+import axios from "axios";
+
+const config = {
+  headers: { "Content-Type": "application/json" },
+  withCredentials: true,
+};
+
+export default function* loginSaga() {
+
+}
+
+function* loginUser(action) {
+    try {
+        
+    } catch (err) {
+        
+    }
+}
---
+++ server/modules/authentication-middleware.js
@@ -0,0 +1,9 @@
+function rejectUnauthenticated(req, res, next) {
+    if(req.isAuthenticated()) {
+        next();
+    } else {
+        res.sendStatus(403);
+    }
+}
+
+module.exports = { rejectUnauthenticated };
