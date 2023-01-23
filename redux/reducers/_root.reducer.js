55a4a955640fdde24046e74c5b3577ab61a062fc
David Lindberg <david.wesley.lindberg@gmail.com>
2023-01-23 14:17:16 -0600

    deleted the user reducer, access to user info will be from local storage

--- redux/reducers/_root.reducer.js
+++ redux/reducers/_root.reducer.js
@@ -1,9 +1,7 @@
 import { combineReducers } from "redux";
-import user from "./user.reducer";
 import feedbackReducer from "./feedback.reducer";
 
 const rootReducer = combineReducers({
-  user,
   feedbackReducer,
 });
 
