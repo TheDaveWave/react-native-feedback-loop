fbcffbca7f392e77145f49eb65f24b3cdc3d2945
David Lindberg <david.wesley.lindberg@gmail.com>
2023-01-23 14:15:11 -0600

    removed a majority of sagas that are no longer needed due to local storage and context

--- redux/sagas/_root.saga.js
+++ redux/sagas/_root.saga.js
@@ -1,9 +1,5 @@
-import { all } from "redux-saga/effects";
-import loginSaga from "./login.saga";
-import registrationSaga from "./registration.saga";
-import userSaga from "./user.saga";
 import feedbackSaga from "./feedback.saga";
 
 export default function* rootSaga() {
-  yield all([loginSaga(), registrationSaga(), userSaga(), feedbackSaga()]);
+  yield feedbackSaga();
 }
